e014a949c8e14ef6c661ebe14c4579f4f09a9841
Dijination-Saad <saadshahrukh141@gmail.com>
2026-01-05 03:01:11 +0500

    Update server.js

--- my-app/pages/api/server.js
+++ my-app/pages/api/server.js
@@ -1,6 +1,5 @@
-const serverless = require('serverless-http');
-const app = require('../../server');
-
-const handler = serverless(app);
-
-module.exports = (req, res) => handler(req, res);
+// Deprecated: migrated to native Next.js API routes
+// This file is no longer used. All endpoints are under /api/*
+module.exports = (req, res) => {
+  res.status(410).json({ message: 'This endpoint is deprecated. Use native Next.js API routes instead.' });
+};
