005fa147d7a46a2e06edadeb932d3cda48c42be1
Dijination-Saad <saadshahrukh141@gmail.com>
2026-01-05 14:31:42 +0500

    fix problem related to deployment

--- my-app/src/pages/api/server.js
+++
@@ -1,5 +0,0 @@
-// Deprecated: migrated to native Next.js API routes
-// This file is no longer used. All endpoints are under /api/*
-module.exports = (req, res) => {
-  res.status(410).json({ message: 'This endpoint is deprecated. Use native Next.js API routes instead.' });
-};
